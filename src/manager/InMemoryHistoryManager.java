a2a4c217584aa93bcfcf2640974bc874c9a7db74
Antroverden <Antroverden@yandex.ru>
2022-12-13 01:00:38 +0300

    refactor: изменить структуру хранения истории

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -2,15 +2,17 @@ package manager;
 
 import tasks.Task;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    private final List<Task> history = new ArrayList<>();
+    private static final int LIMIT = 10;
+    private final List<Task> history = new LinkedList<>();
 
+    @Override
     public void add(Task task) {
-        if (history.size() == 10) {
+        if (history.size() >= LIMIT) {
             history.remove(0);
         }
         history.add(task);
